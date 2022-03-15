f53ed0dfcb65a1a523440c3132aa80c7b8eb2cba
TheSelim9043 <selim9043@gmail.com>
2022-03-16 00:24:24 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+We are using visual studio code, and run terminal with chrome.
+
+And we are using Console with inspect in live server.
---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Hello World!")
+console.log("Testlog")
+const age = 22 ;
+const name = "Selim" ;
+
+alert(name+age);
+
+let student = {
+
+    name: "Nate",
+    surname: "Diaz",
+    birthday: 1993,
+    degree: "master",
+  
+    }
+  
+    console.log(student);
+
